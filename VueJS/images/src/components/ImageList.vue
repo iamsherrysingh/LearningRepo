d73afe77099a759ad0f2e413e2722c13d41e2c2f
Sherry <iamsherrysingh@gmail.com>
2020-09-07 12:36:33 -0400

    VueJS: Image Upload project is complete

---
+++ VueJS/images/src/components/ImageList.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div>
+        <div v-if="isLoggedIn">
+            <img class="image-container"
+                v-for="image in allImages" 
+                v-bind:key="image.link" 
+                v-bind:src= "image.link"/>
+        </div>
+        <h2 v-else>Log in to get started!</h2>
+    </div>
+</template>
+
+
+<script>
+import { mapActions , mapGetters } from 'vuex'
+
+export default {
+    name : 'ImageList',
+    computed : mapGetters (['allImages'  , 'isLoggedIn']),
+    methods : mapActions(['fetchImages']),
+    created(){
+        this.fetchImages()
+    }
+}
+</script>
+
+
+<style scoped>
+.image-container{
+    column-count: 3;
+    column-gap: 0;
+}
+
+img{
+    max-width: 50%;
+    padding: 5px
+}
+</style>
