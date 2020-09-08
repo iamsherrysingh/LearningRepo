f3afb024434dcf4550383c18ee97ad9fc1b56f70
Sherry <iamsherrysingh@gmail.com>
2020-09-08 09:42:12 -0400

    SpringBoot: Added Sample APIs

---
+++ apistore/src/main/java/com/sherry/apistore/todoapp/ToDoApp.java
@@ -0,0 +1,13 @@
+package com.sherry.apistore.todoapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ToDoApp {
+
+	@GetMapping("/todoapp")
+	private String sayHello() {
+		return "Hello from ToDo App";
+	}
+}
