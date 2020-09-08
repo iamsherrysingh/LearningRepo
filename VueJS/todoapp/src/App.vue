c3223b37d479afd5feeb500c67b45c962466f2b9
Sherry <iamsherrysingh@gmail.com>
2020-09-08 15:45:33 -0400

    VueJS: Fresh Start to todoapp

--- VueJS/todoapp/src/App.vue
+++ VueJS/todoapp/src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div id="app">
-    <div>
-      <AppHeader></AppHeader>
-      <div>
-        <List>
-          <div><ListItem> </ListItem></div>
-        </List>
-      </div>
-
-    </div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader'
-import List from './components/List'
-import ListItem from './components/ListItem'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    AppHeader,
-    List,
-    ListItem
+    HelloWorld
   }
 }
 </script>
 
 <style>
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
